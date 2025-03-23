fddfe517f7621257c95c95dfd87037b31cf614ed
avi312singh <avi312singh@gmail.com>
2025-03-23 22:49:11 +0000

    gatsby dark mode fix

---
+++ gatsby-ssr.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { DarkThemeProvider } from "./src/components/DarkThemeContext/DarkThemeContext";
+
+export const wrapRootElement = ({ element }) => {
+  return <DarkThemeProvider>{element}</DarkThemeProvider>;
+};
--- src/components/DarkThemeContext/DarkThemeContext.js
+++ src/components/DarkThemeContext/DarkThemeContext.js
@@ -3,13 +3,19 @@ import React, { createContext, useContext, useState, useEffect } from "react";
 const DarkThemeContext = createContext();
 
 export const DarkThemeProvider = ({ children }) => {
-  const [darkMode, setDarkMode] = useState(() => {
-    const savedTheme = localStorage.getItem("darkMode");
-    return savedTheme ? JSON.parse(savedTheme) : false;
-  });
+  const [darkMode, setDarkMode] = useState(false);
 
   useEffect(() => {
-    localStorage.setItem("darkMode", JSON.stringify(darkMode));
+    if (typeof window !== "undefined") {
+      const savedTheme = localStorage.getItem("darkMode");
+      setDarkMode(savedTheme ? JSON.parse(savedTheme) : false);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      localStorage.setItem("darkMode", JSON.stringify(darkMode));
+    }
   }, [darkMode]);
 
   return (
